324a0e840fd7f68f52a65e8324dce19c8f9615b2
workspchao <worksp@qq.com>
2015-12-16 17:15:25 +0800

    upload file debug

--- application/core/Base_Controller.php
+++ application/core/Base_Controller.php
@@ -701,7 +701,9 @@ class Base_Controller extends CI_Controller {
             $extName = end(explode(".", $fullName));
             $this->dump($extName);
             $extName = strtolower($extName);
+            $this->dump($extName);
             $newName = $newName . '.' . $extName;
+            exit();
             
             $ci_result = $this->_ci_upload($field, $newName, $uploadPath, $allowedTypes, $maxSize);
             if($ci_result === false)
